136a83f49e11d8560b1a88c089ef3852c1f08228
denis <denis055023@gmail.com>
2022-11-06 20:47:53 +0100

    test

---
+++ Homeworks/lesson_2/home_task.py
@@ -0,0 +1,43 @@
+# hals = int(input("Enter your hals: "))
+# if hals <= 0:
+#     print("False")
+# else:
+#     print("True")
+
+# num = int(input("Enter your number: "))
+# if num % 2 == 0:
+#     print("even")
+# else:
+#     print("odd")
+
+# year = int(input("Enter your year: "))
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print('Високосный')
+#         else:
+#             print('Невисокосный')
+#     else:
+#         print('Високосный')
+# else:
+#     print('Невисокосный')
+
+# if not year%4 and year%100 or not year%400:
+#     print('Високосный')
+# else:
+#     print('Невисокосный')
+# text = input("Enter your text: ")
+# num = int(input('Enter the number of repetitions: '))
+# for i in range(1, num+1):
+#     print(i, text)
+
+working_hours = int(input("Enter the working hours: "))
+prise_of_hour = int(input("Enter the prise of one hour: "))
+
+
+def cost_of_work(working_hours, prise_of_hours):
+    result = working_hours * prise_of_hours
+    return result
+
+
+print("You yerned " + str(cost_of_work(working_hours, prise_of_hour)) + " " + "today !")
--- lesson1.2.py
+++ lesson1.2.py
@@ -9,11 +9,27 @@
 #     if i == 3:
 #         continue
 
-for i in range(0, 2, 1):
-    for j in range(10):
-        print(j, end = " ")
+# for i in range(0, 2, 1):
+#     for j in range(10):
+#         print(j, end = " ")
+
+# user_age = 50
+# if user_age == 21:
+#     print("You can enter")
+# elif user_age >= 45:
+#     print("Go away")
+
+def test(num, n):
+    return num**n
+
+result = test(2, 3)
 
 
+def more_power(result):
+    if result == 8:
+        print("Yo are hiro")
+
+more_power(result)
 
 
 
